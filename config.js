1ad7744d1505e118e5aa3826d81380410340ec8a
ReedaAl <71945696+ReedaAl@users.noreply.github.com>
2020-10-24 20:04:49 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    prefix: "_",
+    youtubekey: "AIzaSyDVu6KC4RGTrt_wHAHfBOUTU4-Wh8VFu-8" //لازم تغيره
+}
+
+//هذه رابط كيفيه تغيير API
+// https://www.youtube.com/watch?v=IqXELl1tdwA&t=1s
